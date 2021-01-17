8bdd0afc8ed9962aafb857de5b2fd6dfd6c50145
lenovo <lenovo@DESKTOP-OBJUSFO>
2021-01-17 22:27:51 +0300

    16 ocak ders sonu

---
+++ Metotlar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("---------");
+            }
+
+            Console.WriteLine("---------Metotlar---------");
+
+            //instance-örnek oluşturma
+            //encapsulation-kapsülleme (ayrı ayrı yazacağın ve düzensiz olan yapıyı birlikte yazıp düzenli hale getiririz.)
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12);
+        }
+    }
+}
