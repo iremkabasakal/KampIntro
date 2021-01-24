6290b05e30d9d178d60424045db61faab85d76ad
lenovo <lenovo@DESKTOP-OBJUSFO>
2021-01-24 18:06:42 +0300

    24.01.2021

---
+++ OOP2/CustomerManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Manager Sınıflar: İş sınıfları(business classes): kuralları yazarız.
+    class CustomerManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            
+        }
+    }
+}
---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //Method İnjection: Kredi türü ve log türünü enjekte ediyoruz.
+        public void BasvuruYap (IKrediManager krediManager, ILoggerService loggerService)
+        {
+            krediManager.Hesapla();
+            loggerService.Log();
+
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
