faf108f6929cfcd2cba1ec58b857a40846e044fe
lenovo <lenovo@DESKTOP-OBJUSFO>
2021-01-28 22:52:08 +0300

    28.01.2021

---
+++ GameProject/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    interface IOrderService
+    {
+        void Add(Gamer gamer);
+
+    }
+}
---
+++ GameProject/OrderManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class OrderManager : IOrderService
+    {
+        ICampaignService _campaignService;
+
+        public OrderManager(ICampaignService campaignService)
+        {
+            _campaignService = campaignService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            
+            Console.WriteLine("Sipariş alındı");
+        }
+
+        public void Order(Gamer gamer)
+        {
+            if (_campaignService(Add)="Black Friday"
+           
+            Console.WriteLine(gamer.FirstName);
+        }
+}
